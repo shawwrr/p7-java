41b9244389b63eeba8a6174df7e1a048ff5c9d47
shawwrr <shhaworkk@gmail.com>
2026-02-27 11:36:26 +0530

    Add files via upload

---
+++ p7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class p7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = sc.nextInt();
+        System.out.println("enter second number");
+        int b = sc.nextInt();
+        if(a>b){
+            System.out.println("first number is greater");
+        
+        }
+        else{
+            System.out.println("second number is greater");
+        }
+
+        
+    }
+}
